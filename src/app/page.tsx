2efcf8a4e726dc92ad7a1033582cbc7405dd0930
Anuththara99 <anuththara.s@hsenidmobile.com>
2025-06-27 00:01:36 +0530

    Added main sections to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Hero from "@/components/portfolio/Hero";
+import Projects from "@/components/portfolio/Projects";
+import { Skills } from "@/components/portfolio/Skills";
+import { AITools } from "@/components/portfolio/AITools";
+import { Contact } from "@/components/portfolio/Contact";
+import { About } from "@/components/portfolio/About";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
+      <Projects/>
+      <Skills />
+      <AITools />
+      <Contact />
     </main>
   );
 }
